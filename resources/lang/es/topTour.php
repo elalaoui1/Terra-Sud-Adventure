1869bcc089ab0fc2cc268f0b653bcd698611d80f
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:01 +0100

    add Spanish language file for topTour section with titles and descriptions

---
+++ resources/lang/es/topTour.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'min_title' => 'Experiencias Destacadas',
+    'title' => 'Viajes Más Codiciados',
+    'min_slug' => 'Descubre nuestra colección de experiencias de viaje inmersivas, cuidadosamente diseñadas por los expertos de Terra Sud Adventures.',
+    'tour_days' => 'Días',
+    'tour_btn_book' => 'Reservar',
+];
