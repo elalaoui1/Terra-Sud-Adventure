321e78e6f6f551c10046cfc0258b1b9c980667fb
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:31 +0100

    add French language file for tour types with titles and descriptions

---
+++ resources/lang/fr/tourType.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Découvrez Nos Types de Circuits',
+    'tours' => 'Randonnées & Treks',
+    'tours_description' => 'Partez à la découverte des merveilles naturelles du Maroc avec les randonnées guidées de Terra Sud Adventures.',
+
+    'private_tours' => 'Circuits & Voyages Privés',
+    'private_tours_description' => 'Profitez d’expériences personnalisées avec des circuits privés adaptés à vos envies.',
+
+    'excursions' => 'Excursions',
+    'excursions_description' => 'Des aventures de courte durée, de quelques heures à une journée, pour découvrir les joyaux du Maroc.',
+
+    'events' => 'Thèmes & Événements',
+    'events_description' => 'Des voyages thématiques axés sur la culture, la nature ou des événements spéciaux avec Terra Sud Adventures.',
+
+    'custom_tour' => 'Voyage Sur-Mesure',
+    'custom_tour_description' => 'Créez le voyage marocain de vos rêves avec un itinéraire sur-mesure conçu par nos experts.',
+];
+
+
