71fe9c1afda4f5611d4e1510326148aea85235bd
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:18:21 +0100

    refactor HomeController to fetch highlighted tours with minimum adult price

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,10 +10,14 @@ class HomeController extends Controller
     //
     public function index()
     {
-        // get the tours data have highlights is true
-        $tours = Tour::where('is_highlited', true)->get();
+        // get the tours data have highlights is true with min
+
+        // $tours = Tour::where('is_highlited', true)->get();
+        $tours = Tour::where('is_highlited', true)
+                ->withMin('tourPrices', 'adult_price')
+                ->get();
         // dd($tours);
-        // $tours = \App\Models\Tour::with('highlights')->get();
+        
         return view('frontend.home',[
             'tours' => $tours,
         ]);
