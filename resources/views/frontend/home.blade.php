2250bcb1bfa96b2ad32183ab13ab0d8ca355dc97
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:40 +0100

    add Swiper integration and update sections in home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,6 +1,8 @@
 @extends('frontend.layouts.app')
 @section('title', 'Terra Sud Adventures')
 @section('styles')
+<!-- Swiper CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 @endsection
 
@@ -13,10 +15,57 @@
 {{-- top tours section --}}
 @include('frontend.components.home.topTours',['tours' => $tours])
 
+{{-- about section --}}
+@include('frontend.components.home.about')
+
+{{-- tour Types section  --}}
+@include('frontend.components.home.tourTypes')
+
+{{-- about number 2 --}}
+@include('frontend.components.home.about-n2')
+
+{{-- partners section --}}
+@include('frontend.components.home.partners')
+
+
+
+
+
+
+
 @endsection
 
 
 
 @section('scripts')
+
+
+
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<!-- Swiper Init -->
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    new Swiper('.mySwiper', {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  });
+</script>
 
 @endsection
