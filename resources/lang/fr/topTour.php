3d4fd220ec89bf2903fc255a1f97aae17c2bd489
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:26 +0100

    add French language file for topTour section with titles and descriptions

---
+++ resources/lang/fr/topTour.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'min_title' => 'Expériences à la Une',
+    'title' => 'Voyages les Plus Prisés',
+    'min_slug' => 'Découvrez notre collection d’expériences de voyage immersives, soigneusement conçues par les experts de Terra Sud Adventures.',
+    'tour_days' => 'Jours',
+    'tour_btn_book' => 'Réserver',
+];
