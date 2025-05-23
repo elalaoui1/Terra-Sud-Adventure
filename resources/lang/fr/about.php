df09a0b7836d270fb88aa4fbf7102f488d1cb0a3
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:13 +0100

    add French language file for about section with company information and services

---
+++ resources/lang/fr/about.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'min_title' => 'À propos de notre voyage',
+    'title' => 'Créer des moments de voyage inoubliables',
+    'min_slug' => "Terra Sud Adventures crée des voyages riches de sens et durables à travers le Maroc, alliant expertise locale et voyages véritablement personnalisés.",
+    'award' => 'Meilleure agence de voyage',
+    'award_slug' => 'au Maroc',
+    'about_title' => 'Notre vision',
+    'name_company' => 'Terra Sud',
+    'about_us' => "Terra Sud Adventures est une agence de voyage à taille humaine, spécialisée dans les aventures immersives et authentiques à travers les plus belles régions du Maroc.
+                Depuis 2005, nous guidons les voyageurs à travers les montagnes de l’Atlas, le désert du Sahara,
+                et les villages traditionnels, avec des circuits sur mesure pour tous les âges et niveaux.
+                Randonnées, treks, balades à dos de dromadaire, circuits en 4x4 ou découvertes culturelles,
+                chaque itinéraire est conçu avec soin, combinant expertise locale et hospitalité sincère.
+                Faites de votre rêve marocain une expérience inoubliable.",
+    'Journeys' => 'Voyages',
+    'Customer_satisfaction' => 'Satisfaction client',
+    'experience' => "Années d'expérience",
+    'about_us_n2' => "<strong class='text-[#FF681A]'>Terra Sud Adventures</strong> est une agence de voyage à taille humaine spécialisée dans l’organisation de voyages d’aventure au travers les plus belles régions du Maroc. Elle propose aux amoureux de la nature, d’authentiques itinéraires, de nombreuses randonnées et des trekkings adapté à tous les niveaux et tous les âges.
+                <br><br>
+                    Selon vos désirs et le temps qui vous est imparti, nous vous offrons un large choix d’activités et de loisirs, sur des périodes allants d’une demi-journée à plusieurs jours. Que vous choisissiez de parcourir les pistes de l’Atlas et du grand sud marocain en 4×4 ou en VTT, de surfer sur les vagues de l’atlantique, de faire une ballade dans le désert à dos de dromadaire ou à pieds, de gravir les plus hauts sommets de l’Atlas, de visiter et explorer les médinas du Maroc ou bien de faire séjourner dans un lieu sublime en alternant visite et détente. Terra Sud vous conseille et vous accompagne tout au long de votre séjour.
+                <br><br>
+                    Pour votre sécurité tous nos guides ont reçu une formation professionnelle et nos moyens de transports on été agréés par le Ministère des Transports et du Tourisme.",
+    'hiking' => 'RANDONNÉES',
+    'tours' => 'TOURS',
+    'family_tour' => 'TOURS EN FAMILLE',
+    'cycling' => 'CYCLISME',
+    'horse_riding' => 'CHEVAL',
+    'multi_activity' => 'MULTI-ACTIVITÉ',
+
+];
