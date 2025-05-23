9965d8e7105bab1d14f16b537e9b2239a89b5f47
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:19:32 +0100

    add topTour language file for featured experiences and tours

---
+++ resources/lang/en/topTour.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'min_title' => 'Featured Experiences',
+    'title' => 'Most Coveted Journeys',
+    'min_slug' => 'Discover our curated collection of immersive travel experiences, crafted with care by the experts at Terra Sud Adventures.',
+    'tour_days' => 'Days',
+    'tour_btn_book' => 'Book Now',
+
+
+];
