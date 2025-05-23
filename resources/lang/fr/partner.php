c731f4a30de11b399320cf76e65c554d5b0f2c3f
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:20:18 +0100

    add French language file for partner section with titles and subtitles

---
+++ resources/lang/fr/partner.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'partner_title' => 'Partenaire de confiance des leaders du secteur',
+    'partner_subtitle' => 'En collaboration avec des marques mondiales pour offrir des expériences de voyage exceptionnelles',
+
+];
