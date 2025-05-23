1c7e498ca81ebabb283005007f7080231f74be6e
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:19:55 +0100

    add Spanish language file for partner section with titles and subtitles

---
+++ resources/lang/es/partner.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'partner_title' => 'Con la confianza de líderes del sector',
+    'partner_subtitle' => 'Colaborando con marcas globales para ofrecer experiencias de viaje excepcionales',
+
+];
