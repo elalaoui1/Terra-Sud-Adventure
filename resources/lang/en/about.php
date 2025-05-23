42096609cd307803ae906901e7892c4a709d9526
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:19:20 +0100

    add about language file with company information and services

---
+++ resources/lang/en/about.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'min_title' => 'About Our Journey',
+    'title' => 'Crafting Unforgettable Travel Moments',
+    'min_slug' =>'Terra Sud Adventures has crafted meaningful and sustainable journeys across Morocco, blending local expertise with truly personalized travel.',
+    'award' =>'Best Travel Agency',
+    'award_slug' =>'in morocco',
+    'about_title' => 'Our Vision',
+    'name_company' => 'Terra Sud',
+    'about_us' => "Terra Sud Adventures is a human-scale travel agency,
+                specializing in immersive and authentic journeys through Morocco's most breathtaking regions.
+                Since 2005, we’ve been guiding travelers across the Atlas Mountains, the Sahara Desert,
+                and picturesque traditional villages, offering tailor-made adventures for all levels and ages.
+                From trekking and hiking to camel rides, 4x4 tours, and cultural discoveries,
+                every itinerary is crafted with care, combining local expertise and genuine hospitality.
+                Let us turn your Moroccan dream into an unforgettable experience.",
+    'Journeys' => 'Journeys',
+    'Customer_satisfaction' => 'Customer satisfaction',
+    'experience' => 'Years of Experience',
+    'about_us_n2' => "<strong class='text-[#FF681A]'>Terra Sud Adventures</strong> is a small-scale travel agency specializing in organizing adventure trips across the most beautiful regions of Morocco. It offers nature lovers authentic itineraries, numerous hikes, and treks suitable for all levels and ages.
+                    <br><br>
+                    Depending on your wishes and the time you have available, we offer a wide range of activities and leisure options, ranging from half-day trips to multi-day adventures. Whether you choose to explore the tracks of the Atlas Mountains and the great Moroccan south by 4x4 or mountain bike, surf the waves of the Atlantic, take a walk in the desert on camelback or on foot, climb the highest peaks of the Atlas, visit and explore the medinas of Morocco, or stay in a beautiful place alternating visits and relaxation — Terra Sud will guide and support you throughout your stay.
+                    <br><br>
+                    For your safety, all our guides have received professional training, and our means of transport are approved by the Ministry of Transport and Tourism.",
+    'hiking' => 'Hiking',
+    'tours' => 'TOURS',
+    'family_tour' => 'FAMILY TOURS',
+    'cycling' => 'CYCLING',
+    'horse_riding' => 'HORSE RIDING',
+    'multi_activity' => 'MULTI-ACTIVITY',
+
+
+
+];
