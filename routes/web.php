1b8f56f2bc49bffda8688f7e7b38d60a52538cfc
elalaoui <elalaouiabdelkarim22@gmail.com>
2025-05-23 17:21:49 +0100

    feat: add route for Treks & Hikes page to enhance navigation

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TreksHikesController;
 
 // Route::get('/', function () {
 //     return view('frontend.home');
 // });
 Route::get('/', [HomeController::class , 'index'])->name('home');
+Route::get('/treks_hikes', [TreksHikesController::class , 'index'])->name('TreksHikes');
 
 Route::get('/lang/{locale}', function ($locale) {
     if (! in_array($locale, ['en', 'fr', 'es'])) {
